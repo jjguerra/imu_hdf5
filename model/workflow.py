6b84940bc291f7d1066b83a3b4a85bd59b09aa41
guerrajorge <jorgej.guerra@gmail.com>
2016-10-14 19:30:34 -0400

    only grabing dataset with a name

--- model/workflow.py
+++ model/workflow.py
@@ -11,8 +11,8 @@ import datetime
 def imu_algorithm(dataset_directory='', algorithm='', quickrun='', program_path=''):
 
     # list all the files where the sensordata is stored
-    dataset_files = os.listdir(dataset_directory)
-    # dataset_files = ['processed_sensordata.hdf5']
+    # dataset_files = os.listdir(dataset_directory)
+    dataset_files = ['processed_sensordata.hdf5']
 
     # need to put all the files in the same h5py file
     if len(dataset_files) < 1:
