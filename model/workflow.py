b0ee0b149829872c58bb9fe2ea156105e3b55050
guerrajorge <jorgej.guerra@gmail.com>
2016-10-31 12:18:37 +0000

    fixing filaname error

--- model/workflow.py
+++ model/workflow.py
@@ -61,8 +61,12 @@ def imu_algorithm(dataset_directory='', algorithm='', quickrun='', program_path=
     for user_index, user_info in enumerate(h5_file_object.iterkeys()):
 
         # run test on control users only
+        # if 'pilot' in user_info and \
+        #        ('HS00' in user_info or 'N537' in user_info or 'Q130' in user_info or 'Q430' in user_info or 'Q435' in
+        #            user_info):
+
         if 'pilot' in user_info and \
-                ('HS00' in user_info or 'N537' in user_info or 'Q130' in user_info or 'Q430' in user_info or 'Q435' in
+                ('Q439' in user_info or 'Q568' in user_info or 'Q615' in user_info or 'Q616' in user_info or 'Q617' in
                     user_info):
             # defining train dataset and labels array
             c_filename = 'training_testing_file_' + str(user_info) + '_' + datetime.now().strftime('%Y%m%d%H%M%S')\
@@ -114,23 +118,23 @@ def imu_algorithm(dataset_directory='', algorithm='', quickrun='', program_path=
                 n_inner_column -= 1
 
                 if inner_user != user and 'paretic' not in user_info_inner:
-                    # get type of activity i.e. horizontal, vertical or freedly
-                    type_activity = label_object.check_type_activity(str(activity))
-                    inner_type_activity = label_object.check_type_activity(str(inner_activity))
+                #    # get type of activity i.e. horizontal, vertical or freedly
+                #    type_activity = label_object.check_type_activity(str(activity))
+                #    inner_type_activity = label_object.check_type_activity(str(inner_activity))
 
-                    # if testing on the feeding activity or 'freedly' activities, always add other training activities
-                    # TO DO: I can add training on the same user but different activities to see if that improves the
-                    # feeding activity
-                    if (type_activity == 'freedly') and (user != inner_user):
-                        adding = True
+                #    # if testing on the feeding activity or 'freedly' activities, always add other training activities
+                #    # TO DO: I can add training on the same user but different activities to see if that improves the
+                #    # feeding activity
+                #    if (type_activity == 'freedly') and (user != inner_user):
+                #        adding = True
 
-                    # removing all the users with the freedly activities, check if they have the same type of activity
-                    # and add other users
-                    # TO DO: I can add training on the same user but different activities to see if that improves the
-                    # feeding activity
-                    elif (type_activity != 'freedly') and (type_activity == inner_type_activity) and \
-                            (user != inner_user):
-                        adding = True
+                #    # removing all the users with the freedly activities, check if they have the same type of activity
+                #    # and add other users
+                #    # TO DO: I can add training on the same user but different activities to see if that improves the
+                #    # feeding activity
+                #    if (type_activity != 'freedly') and (type_activity == inner_type_activity):
+                #        adding = True
+                    adding = True
 
                 if adding:
                     # get the size of the dataset because it will be passed as an parameter to the hmm
